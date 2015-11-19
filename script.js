393a755136480c4883758d864a54c930b56670a8
Chelsea Parkkila <cmparkkila@gmail.com>
2015-11-19 15:32:59 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+//building routing module with dependency of ngRoute
+
+var app = angular.module('theRoutes', ['ngRoute']);
+
+//configuring routes
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'catView.html',
+			controller: 'routeController'
+		});
+	
+	$routeProvider
+		.when('/fancy', {
+			templateUrl: 'fancyView.html',
+			controller: 'routeController'
+		});
+
+	$routeProvider
+		.when('/dog', {
+			templateUrl: 'dogView.html',
+			controller: 'routeController'
+		});
+	$routeProvider
+		.when('/grandma', {
+			templateUrl: 'cat2view.html',
+			controller: 'routeController'
+		});
+	$routeProvider
+		.when('/strongest', {
+			templateUrl: 'cat3view.html',
+			controller: 'routeController'
+		})
+
+		.otherwise({ redirectTo: '/' });
+
+	$locationProvider.html5Mode(true);
+});
+
+//building the controller
+app.controller('routeController', function(){
+
+});
